247ddc620d8f7b87c7625272492c73d4cb1ffaca
agustiodwiputra <tiocfcmantab@gmail.com>
2019-08-07 13:44:06 +0700

    moss

--- kendaraan.php
+++ kendaraan.php
@@ -19,35 +19,35 @@ class motor{
 		echo'bahan_bakar :'.$this->bahan_bakar.",";;
 	}
 }
-$motor = new kendaraan();
+$motor = new motor();
 $motor->setmerk('mio j');
 $motor->settransportasi('darat');
 $motor->setbahan_bakar('bensin');
 $motor->cetak();
 echo "<br>";
 
-$motor = new kendaraan();
+$motor = new motor();
 $motor->setmerk('kawasaki');
 $motor->settransportasi('darat');
 $motor->setbahan_bakar('pertamax');
 $motor->cetak();
 echo "<br>";
 
-$motor = new kendaraan();
+$motor = new motor();
 $motor->setmerk('supra');
 $motor->settransportasi('darat');
 $motor->setbahan_bakar('bensin');
 $motor->cetak();
 echo "<br>";
 
-$motor = new kendaraan();
+$motor = new motor();
 $motor->setmerk('vario 150');
 $motor->settransportasi('darat');
 $motor->setbahan_bakar('bensin');
 $motor->cetak();
 echo "<br>";
 
-$motor = new kendaraan();
+$motor = new motor();
 $motor->setmerk('satria fu');
 $motor->settransportasi('darat');
 $motor->setbahan_bakar('pertamax');
